d5f43dc427d865dd7c72cc4c4b9da1b9cf034f31
rwbakerUMASS <rwbaker@umass.edu>
2022-04-27 13:13:09 -0400

    Create data_gathering.py

---
+++ data_gathering.py
@@ -0,0 +1,37 @@
+from hx711 import HX711
+from firebase import Firebase
+import RPi.GPIO as GPIO
+from hx711 import HX711
+import time
+
+config = {
+  "apiKey": "AIzaSyBqmtDdXcCLp4ODEgtkelMj7QWEixxSVOY",
+  "authDomain": "intelli--bed.firebaseapp.com",
+  "databaseURL": "https://intelli--bed-default-rtdb.firebaseio.com/",
+  "storageBucket": "intelli--bed.appspot.com"
+}
+
+referenceUnit=1
+
+sensors = [HX711(20,21),HX711(12,16),HX711(17,27),HX711(5,6)]
+for hx in sensors:
+    hx.set_reading_format("MSB", "MSB") 
+    hx.set_reference_unit(referenceUnit)
+    hx.reset()
+    hx.tare()
+
+fb = Firebase(config)
+
+classification = input("Input activity")
+
+
+while True:
+    timestamp = time.time()
+    vals = []
+    for hx in sensors:
+        vals.append(hx.get_weight(1))
+        hx.power_down()
+        hx.power_up()
+    data={"sensor":vals}
+    print(data)
+    time.sleep(0.1)
