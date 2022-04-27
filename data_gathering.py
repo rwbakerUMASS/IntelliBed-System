8cc9b37685215a5b06a57b0212f9a12a71762643
rwbakerUMASS <rwbaker@umass.edu>
2022-04-27 13:23:57 -0400

    calibration routine

--- data_gathering.py
+++ data_gathering.py
@@ -4,6 +4,12 @@ import RPi.GPIO as GPIO
 from hx711 import HX711
 import time
 
+def calibrateSensors(sensors):
+    baseline = sensors[0].get_weight(10)
+    sensors[1].set_reference_unit(baseline/sensors[1].get_weight(10))
+    sensors[2].set_reference_unit(baseline/sensors[2].get_weight(10))
+    sensors[3].set_reference_unit(baseline/sensors[3].get_weight(10))
+
 config = {
   "apiKey": "AIzaSyBqmtDdXcCLp4ODEgtkelMj7QWEixxSVOY",
   "authDomain": "intelli--bed.firebaseapp.com",
@@ -11,12 +17,12 @@ config = {
   "storageBucket": "intelli--bed.appspot.com"
 }
 
-referenceUnit=1
-
 sensors = [HX711(20,21),HX711(12,16),HX711(17,27),HX711(5,6)]
 for hx in sensors:
     hx.set_reading_format("MSB", "MSB") 
-    hx.set_reference_unit(referenceUnit)
+    hx.set_reference_unit(1)
+calibrateSensors(sensors)
+for hx in sensors:
     hx.reset()
     hx.tare()
 
@@ -33,6 +39,7 @@ while True:
         hx.power_down()
         hx.power_up()
     data={"sensor":vals}
-    fb.addData(timestamp,data)
+    # fb.addData(timestamp,data)
     print(data)
     time.sleep(0.1)
+
