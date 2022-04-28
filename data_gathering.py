9e2e79a19ce30b1976258c57879e344fe5224f4e
rwbakerUMASS <rwbaker@umass.edu>
2022-04-28 16:54:53 -0400

    Update data_gathering.py

--- data_gathering.py
+++ data_gathering.py
@@ -5,29 +5,21 @@ from hx711 import HX711
 import time
 import numpy as np
 
-def findBase(hx,num_samples):
-    vals=[]
-    print("Finding Base Scalar...")
-    hx.set_reference_unit(1)
-    for i in range(num_samples):
-        vals.append(hx.get_weight(5))
-        hx.power_down()
-        hx.power_up()
-        time.sleep(0.1)
-        print(vals[i])
-    mean=np.mean(vals)
-    print("Base = ",mean)
-    return mean
-
-
-def calibrateSensors(sensors):
+def calibrateSensors(sensors,num_samples):
     input("Add weight to center of bed")
-    base = findBase(sensors[0],10)
-    baseline = sensors[0].get_weight(5)
-    sensors[0].set_reference_unit(base)
-    sensors[1].set_reference_unit(base*baseline/sensors[1].get_weight(5))
-    sensors[2].set_reference_unit(base*baseline/sensors[2].get_weight(5))
-    sensors[3].set_reference_unit(base*baseline/sensors[3].get_weight(5))
+    for hx in sensors:
+        vals=[]
+        print("Finding Base Scalar...")
+        hx.set_reference_unit(1)
+        for i in range(num_samples):
+            vals.append(hx.get_weight(5))
+            hx.power_down()
+            hx.power_up()
+            time.sleep(0.1)
+            print(vals[i])
+        mean=np.mean(vals)
+        print("Base = ",mean)
+        hx.set_reference_unit(mean)
 
 config = {
   "apiKey": "AIzaSyBqmtDdXcCLp4ODEgtkelMj7QWEixxSVOY",
@@ -35,7 +27,7 @@ config = {
   "databaseURL": "https://intelli--bed-default-rtdb.firebaseio.com/",
   "storageBucket": "intelli--bed.appspot.com"
 }
-
+print("Initializing HX711s...")
 sensors = [HX711(20,21),HX711(12,16),HX711(17,27),HX711(5,6)]
 print("Setting HX711 Reading Format...")
 for hx in sensors:
